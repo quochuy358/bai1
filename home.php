652f1cb95419288e2ca0bb5a847a65db50000ac2
quochuy358 <vuongquochuy2k8@gmail.com>
2025-02-21 14:50:59 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+$a = 10;  
+$b = 5;   
+
+$tong = $a + $b;      
+$hieu = $a - $b;      
+$tich = $a * $b;     
+$thuong = $a / $b;    
+$du = $a % $b;        
+
+
+echo "Tổng 2 số $a và $b là: $tong<br>";
+echo "Hiệu 2 số $a và $b là: $hieu<br>";
+echo "Tích 2 số $a và $b là: $tich<br>";
+echo "Thương 2 số $a và $b là: $thuong<br>";
+echo "Số dư của phép chia trên là: $du<br>";
+?>
